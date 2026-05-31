ef252c9c6f933dc1444f24c161567e78261f963e
bisham14 <bisham623310777@gmail.com>
2026-06-01 00:40:54 +0530

    Add files via upload

---
+++ html-css-js-project/color_switch/script.js
@@ -0,0 +1,15 @@
+ document.getElementById("red-btn").addEventListener("click", function() {
+            document.body.style.backgroundColor = "#e74c3c";
+        });
+document.getElementById("blue-btn").addEventListener("click", function() {
+            document.body.style.backgroundColor = "#3498db";
+        });
+ document.getElementById("green-btn").addEventListener("click", function() {
+            document.body.style.backgroundColor = "#2ecc71";
+        });
+ document.getElementById("purple-btn").addEventListener("click", function() {
+            document.body.style.backgroundColor = "#9b59b6";
+        });
+document.getElementById("dark-btn").addEventListener("click", function() {
+            document.body.style.backgroundColor = "#2c3e50";
+        });
